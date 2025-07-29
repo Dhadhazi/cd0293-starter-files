d76c4f798adde6878351f1a48d1f5f5f735cccf5
Daniel Hadhazi <dhadhazi@gmail.com>
2025-07-29 15:35:05 +0200

    6 finished

---
+++ 6-SQL_for_Advanced_API/3-Solution-Create_a_Dashboard_Endpoint/src/services/dashboard.ts
@@ -0,0 +1,35 @@
+import Client from "../database";
+
+export class DashboardQueries {
+  async usersWithOrders(): Promise<{ firstName: string; lastName: string }[]> {
+    try {
+      const conn = await Client.connect();
+      const sql =
+        "SELECT first_name, last_name FROM users INNER JOIN orders ON users.id = orders.user_id";
+
+      const result = await conn.query(sql);
+
+      conn.release();
+
+      return result.rows;
+    } catch (err) {
+      throw new Error(`unable get users with orders: ${err}`);
+    }
+  }
+
+  async fiveMostExpensive(): Promise<{ name: string; price: number }[]> {
+    try {
+      const conn = await Client.connect();
+      const sql =
+        "SELECT name, price FROM products ORDER BY price DESC LIMIT 5";
+
+      const result = await conn.query(sql);
+
+      conn.release();
+
+      return result.rows;
+    } catch (err) {
+      throw new Error(`unable get products by price: ${err}`);
+    }
+  }
+}
